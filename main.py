0c647eb81becaad16d5ba1bc61b27e28691bda91
Nuno André Ramos De Lemos <nuno@connecting-software.com>
2024-07-29 12:19:36 +0100

    Removed testing file main.py and build folders

--- main.py
+++
@@ -1,9 +0,0 @@
-from blazingapi.auth.models import User
-from blazingapi.orm.query import Q
-
-r = User.manager.filter(Q(username="admin", id=1) | Q(username="root", id=1))[5:10]
-#r = User.manager.filter(Q(username="admin", id=1) | Q(username="root", id=2)).filter(email="root@gmail.com")
-#r = User.manager.all()[9:]
-for a in r:
-    pass
-
