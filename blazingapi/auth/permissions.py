ee4e2f49a63496940671ea5a2e6fde431cc55090
Nuno André Ramos De Lemos <nuno@connecting-software.com>
2024-08-09 12:56:02 +0100

    Added BasePermission class

--- blazingapi/auth/permissions.py
+++ blazingapi/auth/permissions.py
@@ -1,8 +1,12 @@
 from blazingapi.auth.models import AnonymousUser
 from blazingapi.auth.exceptions import AuthenticationFailedException
+from blazingapi.permissions import BasePermission
 
 
-class IsAuthenticated:
+class IsAuthenticated(BasePermission):
+    """
+    Allows access only to authenticated users.
+    """
 
     def __call__(self, request):
         if not hasattr(request, 'user') or isinstance(request.user, AnonymousUser):
